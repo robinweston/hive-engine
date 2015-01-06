1807a0e3baf3bd6697372d395e44af38caa2ad8a
Robin Weston <rweston@thoughtworks.com>
2015-01-06 09:48:04 +0000

    Add extension to retrieve placed tiles only

--- HiveEngine.Tests.Unit/GridExtensions.cs
+++ HiveEngine.Tests.Unit/GridExtensions.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace HiveEngine.Tests.Unit
 {
     public static class GridExtensions
     {
-        public static IEnumerable<Tile> AllPlaced(this Tile[,] tileGrid)
+        public static IEnumerable<Tile> AsEnumerable(this Tile[,] tileGrid)
         {
             for (var x = 0; x < tileGrid.GetLength(0); x++)
             {
                 for (var y = 0; y < tileGrid.GetLength(1); y++)
                 {
                     var tile = tileGrid[x, y];
-                    if (tile != Tile.None)
-                    {
-                        yield return tile;
-                    }
+                    yield return tile;
                 }
             }
+        }
+
+        public static IEnumerable<Tile> PlacedOnly(this IEnumerable<Tile> tiles)
+        {
+            return tiles.Where(t => t != Tile.None);
         }
     }
 }
