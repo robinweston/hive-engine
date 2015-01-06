5334cd212d89fab182ab52554bb05e8e88d768ab
Robin Weston <rweston@thoughtworks.com>
2015-01-06 17:48:03 +0000

    Added failing test for first black move

--- HiveEngine.Tests.Unit/ValidMoveTests.cs
+++ HiveEngine.Tests.Unit/ValidMoveTests.cs
@@ -7,16 +7,23 @@ namespace HiveEngine.Tests.Unit
     [TestFixture]
     class ValidMoveTests
     {
+        private GameEngine _gameEngine;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _gameEngine = new GameEngine();
+        }
+
         [Test]
         public void at_start_of_game_and_with_only_one_tile_white_has_one_valid_move()
         {
             var grid = GridResourceParser.ParseGrid("empty");
-            var gameEngine = new GameEngine();
 
             var whiteTilesToPlay = new[] { new Tile(TileColor.White, Insect.Queen) };
             var gameState = new GameState(grid, 0, whiteTilesToPlay, Enumerable.Empty<Tile>());
 
-            var validMoves = gameEngine.FindValidMoves(gameState);
+            var validMoves = _gameEngine.FindValidMoves(gameState);
 
             validMoves.Single().Tile.Color.Should().Be(TileColor.White);
             validMoves.Single().Tile.Insect.Should().Be(Insect.Queen);
@@ -30,12 +37,24 @@ namespace HiveEngine.Tests.Unit
             var whiteTilesToPlay = new[] { new Tile(TileColor.White, Insect.Queen), new Tile(TileColor.White, Insect.Ant) };
             var gameState = new GameState(grid, 0, whiteTilesToPlay, Enumerable.Empty<Tile>());
 
-            var gameEngine = new GameEngine();
+            var validMoves = _gameEngine.FindValidMoves(gameState);
 
-            var validMoves = gameEngine.FindValidMoves(gameState);
-
+            validMoves.Count().Should().Be(2);
             validMoves.Should().Contain(m => m.Tile.Color == TileColor.White && m.Tile.Insect == Insect.Queen && m.To.Equals(new Position(0, 0)));
             validMoves.Should().Contain(m => m.Tile.Color == TileColor.White && m.Tile.Insect == Insect.Ant && m.To.Equals(new Position(0, 0)));
+        }
+
+        [Test]
+        public void if_black_has_only_one_tile_they_have_correct_initial_valid_moves()
+        {
+            var grid = GridResourceParser.ParseGrid("single-white-queen");
+
+            var blackTilesToPlay = new[] {new Tile(TileColor.Black, Insect.Queen)};
+            var gameState = new GameState(grid, 1, Enumerable.Empty<Tile>(), blackTilesToPlay);
+
+            var validMoves = _gameEngine.FindValidMoves(gameState);
+
+            validMoves.Count().Should().Be(6);
         }
     }
 }
