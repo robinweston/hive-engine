13e0b4fa45e802713f4209380710be30bb13e456
Robin Weston <rweston@thoughtworks.com>
2015-01-07 12:45:57 +0000

    Black and white moves now use same logic

--- HiveEngine/GameEngine.cs
+++ HiveEngine/GameEngine.cs
@@ -28,71 +28,44 @@ namespace HiveEngine
                     }
                 }
             }
-            else if (gameState.PlayerToPlay == TileColor.White)
-            {
-                var positionsAdjacentToWhiteTiles = new List<Position>();
-                var positionsAdjacentToBlackTiles = new List<Position>();
-                var emptyPositions = new List<Position>();
-
-                for (var x = 0; x < gameState.Grid.Tiles.GetLength(0); x++)
-                {
-                    for (var y = 0; y < gameState.Grid.Tiles.GetLength(1); y++)
-                    {
-                        var tile = gameState.Grid.Tiles[x, y];
-                        if (tile.Color == TileColor.Black)
-                        {
-                            positionsAdjacentToBlackTiles.AddRange(FindAdjacentPositions(new Position(x, y)));
-                        }
-                        else if (tile.Color == TileColor.White)
-                        {
-                            positionsAdjacentToWhiteTiles.AddRange(FindAdjacentPositions(new Position(x, y)));
-                        }
-                        else
-                        {
-                            emptyPositions.Add(new Position(x, y));
-                        }
-                    }
-                }
-
-                var positionsWhiteCanPlay = positionsAdjacentToWhiteTiles.Intersect(emptyPositions).Except(positionsAdjacentToBlackTiles);
-                foreach (var position in positionsWhiteCanPlay)
-                {
-                    foreach (var tile in gameState.WhiteTilesToPlay)
-                    {
-                        yield return new Move(tile, position);
-                    }
-                }
-            }
             else
             {
-                var positionsAdjacentToWhiteTiles = new List<Position>();
-                var positionsAdjacentToBlackTiles = new List<Position>();
+                var positionsAdjacentToCurrentPlayer = new List<Position>();
+                var positionsAdjacentToOpposition = new List<Position>();
                 var emptyPositions = new List<Position>();
 
                 for (var x = 0; x < gameState.Grid.Tiles.GetLength(0); x++)
                 {
                     for (var y = 0; y < gameState.Grid.Tiles.GetLength(1); y++)
                     {
+                        var position = new Position(x, y);
                         var tile = gameState.Grid.Tiles[x, y];
-                        if (tile.Color == TileColor.Black)
+                        if (tile.Color == TileColor.None)
                         {
-                            positionsAdjacentToBlackTiles.AddRange(FindAdjacentPositions(new Position(x, y)));
-                        }
-                        else if (tile.Color == TileColor.White)
-                        {
-                            positionsAdjacentToWhiteTiles.AddRange(FindAdjacentPositions(new Position(x, y)));
+                            emptyPositions.Add(position);
                         }
                         else
                         {
-                            emptyPositions.Add(new Position(x, y));
+                            var adjacentPositions = FindAdjacentPositions(position);
+                            if (tile.Color == gameState.PlayerToPlay)
+                            {
+                                positionsAdjacentToCurrentPlayer.AddRange(adjacentPositions);
+                            }
+                            else
+                            {
+                                positionsAdjacentToOpposition.AddRange(adjacentPositions);
+                            }
                         }
                     }
                 }
 
-                var positionsBlackCanPlay = positionsAdjacentToBlackTiles.Intersect(emptyPositions).Except(positionsAdjacentToWhiteTiles);
-                foreach (var position in positionsBlackCanPlay)
+                var playablePositions = positionsAdjacentToCurrentPlayer.Intersect(emptyPositions).Except(positionsAdjacentToOpposition);
+                var currentPlayerTiles = gameState.PlayerToPlay == TileColor.White
+                    ? gameState.WhiteTilesToPlay
+                    : gameState.BlackTilesToPlay;
+                foreach (var position in playablePositions)
                 {
-                    foreach (var tile in gameState.BlackTilesToPlay)
+                    foreach (var tile in currentPlayerTiles)
                     {
                         yield return new Move(tile, position);
                     }
